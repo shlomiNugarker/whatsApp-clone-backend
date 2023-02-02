33d7b46e8346d2e32465724f5e89993e51505e5b
Shlomi Nugarker <shlomin1231@gmail.com>
2023-02-02 21:36:37 +0200

    server

---
+++ server.ts
@@ -0,0 +1,45 @@
+import express, { Express } from 'express'
+import { json } from 'body-parser'
+import expressSession from 'express-session'
+import path from 'path'
+import cors from 'cors'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const app: Express = express()
+const http = require('http').createServer(app)
+
+const session = expressSession({
+  secret: 'secret session',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false },
+})
+
+app.use(session)
+app.use(json())
+app.use(express.static('public'))
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.resolve(__dirname, 'public')))
+} else {
+  const corsOptions = {
+    origin: [
+      'http://127.0.0.1:3000',
+      'http://localhost:3000',
+      'http://localhost:4200',
+    ],
+    credentials: true,
+  }
+  app.use(cors(corsOptions))
+}
+
+app.get('/**', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
+
+const PORT = process.env.PORT || 3030
+http.listen(PORT, () => {
+  console.log(`⚡️Server is running on port: ${PORT}`)
+})
