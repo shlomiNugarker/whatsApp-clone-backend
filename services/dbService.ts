254d18e087571dd01958f0ead9816732923e5978
Shlomi Nugarker <shlomin1231@gmail.com>
2023-02-02 21:43:48 +0200

    dbService

---
+++ services/dbService.ts
@@ -0,0 +1,31 @@
+import mysql from 'mysql'
+
+export default {
+  runSQL,
+}
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  port: 3306,
+  user: 'root',
+  password: '',
+  database: 'heroes_db',
+  insecureAuth: true,
+})
+
+connection.connect((err: unknown) => {
+  if (err) throw new Error('mySql failed connection')
+  console.log('connected to SQL server')
+})
+
+function runSQL(sqlCommand: any): Promise<any> {
+  return new Promise((resolve, reject) => {
+    connection.query(
+      sqlCommand,
+      function (error: unknown, results: any, fields: any) {
+        if (error) reject(error)
+        else resolve(results)
+      }
+    )
+  })
+}
