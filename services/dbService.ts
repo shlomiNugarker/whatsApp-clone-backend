6a95d10f433e42c94f85cac49f8718350521ffd4
Shlomi Nugarker <shlomin1231@gmail.com>
2023-02-12 15:12:57 +0200

    setTimeout

--- services/dbService.ts
+++ services/dbService.ts
@@ -35,7 +35,7 @@ function handleDisconnect() {
       console.log('db error', err)
       if (err.code === 'PROTOCOL_CONNECTION_LOST') {
         // Connection to the MySQL server is usually
-        handleDisconnect() // lost due to either server restart, or a
+        setTimeout(handleDisconnect, 2000) // lost due to either server restart, or a
       } else {
         // connnection idle timeout (the wait_timeout
         throw err // server variable configures this)
